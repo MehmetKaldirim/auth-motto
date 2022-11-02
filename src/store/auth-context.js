845f1df2f594dfb34366af3318d821979e0d48a3
Mehmet <mathdenizi@gmail.com>
2022-11-02 13:02:26 +0100

    auto logout work after certain time

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+let logoutTimer;
+
 const AuthContext = React.createContext({
   token: "",
   isLoggedIn: false,
@@ -7,20 +9,35 @@ const AuthContext = React.createContext({
   logout: () => {},
 });
 
+const calculateRemainingTime = (expirationTime) => {
+  const currentTime = new Date().getTime();
+  const adjExpirationTime = new Date(expirationTime).getTime();
+
+  const remainingDuration = adjExpirationTime - currentTime;
+  return remainingDuration;
+};
+
 export const AuthContextProvider = (props) => {
-  const [token, setToken] = useState(null);
+  const initialToken = localStorage.getItem("token");
+  const [token, setToken] = useState(initialToken);
 
   const userIsLoggedIn = !!token;
   //it convert truthy or falsy value to true or false (boolean)
   //if token is a string that`s not empty => This will return true
   // if token is a string that is empty, this will return false..
 
-  const loginHandler = (token) => {
-    setToken(token);
-  };
-
   const logoutHandler = () => {
     setToken(null);
+    localStorage.removeItem("token");
+  };
+
+  const loginHandler = (token, expirationTime) => {
+    setToken(token);
+    //dont forget we can use only primitive type in local storage
+    localStorage.setItem("token", token);
+
+    const remainingTime = calculateRemainingTime(expirationTime);
+    setTimeout(logoutHandler, remainingTime);
   };
 
   const contextValue = {
